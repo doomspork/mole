9de00c7837354b2a845f372bb6658428b8dee17b
Sean Callan <seancallan@gmail.com>
2014-10-28 11:58:04 -0700

    Configurable host and port

--- lib/mole/config.rb
+++ lib/mole/config.rb
@@ -1,5 +1,17 @@
 module Mole
   class Config
+    DEFAULT_HOST = 'http://www.orwell.io'
+    DEFAULT_POST = 80
+
     attr_accessor :api_token
+    attr_writer :host, :port
+
+    def host
+      @host || DEFAULT_HOST
+    end
+
+    def port
+      @port || DEFAULT_POST
+    end
   end
 end
--- spec/mole_spec.rb
+++ spec/mole_spec.rb
@@ -11,5 +11,30 @@ describe Mole do
     subject { Mole.config }
 
     its(:api_token) { is_expected.to eql token }
+
+    context '#host' do
+      its(:host) { is_expected.to eql 'http://www.orwell.io' }
+
+      context 'when overridden' do
+        before do
+          Mole.config {|c| c.host = 'http://www.something.com' }
+        end
+
+        its(:host) { is_expected.to eql 'http://www.something.com' }
+      end
+    end
+
+    context '#port' do
+      its(:port) { is_expected.to eql 80 }
+
+      context 'when overridden' do
+        before do
+          Mole.config {|c| c.port = 99 }
+        end
+
+        its(:port) { is_expected.to eql 99 }
+      end
+    end
+
   end
 end
